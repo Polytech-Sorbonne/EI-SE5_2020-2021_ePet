faed6c716b775f9a3aa5812d8e23415c1e8839af
ml2405 <laghameriem24@gmail.com>
2021-02-03 15:03:44 +0100

    base

--- SITE/epet.sql
+++ SITE/epet.sql
@@ -5,6 +5,7 @@
 DROP TABLE IF EXISTS User;
 DROP TABLE IF EXISTS Animal;
 DROP TABLE IF EXISTS Perimeter;
+DROP TABLE IF EXISTS Temperature;
 
 --Creation de tables
 CREATE TABLE User(id INTEGER PRIMARY KEY AUTOINCREMENT,password TEXT NOT NULL,username TEXT NOT NULL,nbanimals INTEGER NOT NULL);
@@ -22,11 +23,9 @@ INSERT INTO Animal(name,type,state,secuperim,owner) VALUES ("tokyo","chien",0,1,
 INSERT INTO Animal(name,type,state,secuperim,owner) VALUES ("chanel","chat",1,1,1);
 INSERT INTO Animal(name,type,state,secuperim,owner) VALUES ("franklin","chien",1,2,2);
 --Insertion de données de localisation
-INSERT INTO Perimeter(x0,y0,x1,y1,x2,y2,x3,y3) VALUES (1,2,5,3,3,3,7,5);
+INSERT INTO Perimeter(x0,y0,x1,y1,x2,y2,x3,y3) VALUES (48,3,5,3,3,3,7,5);
 INSERT INTO Perimeter(x0,y0,x1,y1,x2,y2,x3,y3) VALUES (2,3,1,1,2,4,3,2);
 --Insertion de données de température
 INSERT INTO Temperature(temp,date_insert,animal) VALUES (38.5,CURRENT_TIMESTAMP,1);
 INSERT INTO Temperature(temp,date_insert,animal) VALUES (38.5,CURRENT_TIMESTAMP,2);
 INSERT INTO Temperature(temp,date_insert,animal) VALUES (38.7,CURRENT_TIMESTAMP,3);
-
-
