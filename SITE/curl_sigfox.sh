b7eb97e37f5ffd66444f18f21b48563adb501ea3
Lisa <lisa.hennebelle@etu.upmc.fr>
2021-02-17 18:25:23 +0100

    creation small-rest pour json version POST device

--- SITE/curl_sigfox.sh
+++ SITE/curl_sigfox.sh
@@ -3,4 +3,4 @@
 #device : string, up to 8 char
 #data : string hexadecimal
 
-curl -X POST http://localhost:8888/device/\?device\=\"8459a1\"\&time\=010320\&data\=8a9f6fe10e74cdfffe620880
+curl -X POST http://localhost:8888/device\?device\=\"8459a1\"\&time\=010320\&data\=8a9f6fe10e74cdfffe620880
---
+++ SITE/curl_sigfox_json.sh
@@ -0,0 +1,1 @@
+curl -d @sigfox_json.json -H "Content-Type: application/json"  http://localhost:8888/device
