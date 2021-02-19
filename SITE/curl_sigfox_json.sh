485eb88029f2dc810e3ded0bb3da6e7b783e24e7
Melissa <lebretonmelissa.s@gmail.com>
2021-02-19 19:00:06 +0100

    Suppression doublon

--- SITE/curl_sigfox_json.sh
+++
@@ -1,3 +0,0 @@
-curl -d @sigfox_json.json -H "Content-Type: application/json"  http://localhost:8888/device
-curl -d @sigfox_json2.json -H "Content-Type: application/json"  http://localhost:8888/device
-curl -d @sigfox_json3.json -H "Content-Type: application/json"  http://localhost:8888/device
