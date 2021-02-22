953cbecf5f72df60eaf50c073642e55c49af101c
Melissa <lebretonmelissa.s@gmail.com>
2021-02-22 10:15:31 +0100

    Création dossier visuels

--- CLOUD/SITE/example_requests/curl_sigfox_json.sh
+++ CLOUD/SITE/example_requests/curl_sigfox_json.sh
@@ -1,3 +1,3 @@
 curl -d @sigfox_json.json -H "Content-Type: application/json"  http://localhost:8888/device
-curl -d @sigfox_json2.json -H "Content-Type: application/json"  http://localhost:8888/device
+curl -d @sigfox_json2.json -H "Content-Type: application/json"  http://localhost:8888/device 
 curl -d @sigfox_json3.json -H "Content-Type: application/json"  http://localhost:8888/device
